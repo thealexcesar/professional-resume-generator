d71d377039e1fd1a52f5db59d3888fedc57567c4
Alex Cesar <thealexcesar@gmail.com>
2024-01-17 22:40:50 -0300

    Configure

--- config/application.rb
+++ config/application.rb
@@ -20,23 +20,13 @@ Bundler.require(*Rails.groups)
 
 module ProfessionalResumeGenerator
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    I18n.available_locales = [:en, :es, 'pt-BR']
+    I18n.default_locale = 'pt-BR'
+
     config.autoload_lib(ignore: %w(assets tasks))
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
   end
 end
